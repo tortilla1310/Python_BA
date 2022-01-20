05b24b26f8eeabb974c0ef0a4aba0f347e5f01ec
tortilla <tortilla1310@gmail.com>
2022-01-20 10:13:28 +0100

    additional changes

--- task6.py
+++ task6.py
@@ -1,7 +1,8 @@
-day1 = int(input('how many kilometers a sportsman runs first day: '))
+current_day_distance = int(input('how many kilometers a sportsman runs first day: '))
 distance = int(input('what is the distance: '))
 day = 1
-while day1 <= distance:
-    day1 = 1.1 * day1
+while current_day_distance <= distance:
+    current_day_distance = 1.1 * current_day_distance
+    print( day, 'day', round(current_day_distance,2))
     day +=1
 print('Спортсмен достигнет результата на %.d' % day, 'день')
